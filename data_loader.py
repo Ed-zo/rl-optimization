540882d3457fc57f237432cfc79599ae64a03c0f
David Matis <dmatis01@gmail.com>
2023-09-30 17:15:13 +0200

    Added graph data + loading in torch geometric

---
+++ data_loader.py
@@ -0,0 +1,13 @@
+import torch
+import pandas as pd
+
+def load_node_csv(path, index_col, encoders=None, **kwargs):
+    df = pd.read_csv(path, index_col=index_col, **kwargs)
+    mapping = {index: i for i, index in enumerate(df.index.unique())}
+
+    x = None
+    if encoders is not None:
+        xs = [encoder(df[col]) for col, encoder in encoders.items()]
+        x = torch.cat(xs, dim=-1)
+
+    return x, mapping
