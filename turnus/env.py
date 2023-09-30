5d4395436aa431ce126bba0ebe378b85a5c1d3f8
Ec_@PC <dmatis01@gmail.com>
2023-09-30 22:05:43 +0200

    Preparation of environment

--- turnus/env.py
+++ turnus/env.py
@@ -1,11 +1,29 @@
 import torch
+from torch_geometric.data import Data
+
+MAX_VEHICLES = 50
 
 class Env:
-    def __init__(self):
-        
+    def __init__(self, graph: Data, device = 'cpu'):
+        self.graph = graph
+        self.device = device
+        self.vehicleID = 1
+
+        self.reset()
+
     def reset(self):
+        self.graph.x[:, 6] = 0
+        self.vehicleID = 1
 
     def compute_objective_function(self):
 
-    def step(self, actions):
+    # Return next state, mask, reward, and terminal state
+    def step(self, action) -> (torch.Tensor, torch.Tensor):
+        self.graph.x[action, 6] = 1
+        mask = self.graph.x[:, 6] == 0 
+
+        reward = 0 
+        terminal = ~torch.any(mask).item()
+
+        return self.graph, mask, reward, terminal
         
---
+++ turnus/graph_utils.py
@@ -0,0 +1,47 @@
+from torch_geometric.data import Data
+import torch_geometric.transforms as T
+import torch
+import pandas as pd
+import numpy as np
+
+def load_node_csv(path: str, index_col: str, encoders=None, **kwargs):
+    df = pd.read_csv(path, index_col=index_col, **kwargs)
+    mapping = {index: i for i, index in enumerate(df.index.unique())}
+
+    x = None
+    if encoders is not None:
+        xs = [encoder(df[col]) for col, encoder in encoders.items()]
+        x = torch.cat(xs, dim=-1)
+        df = df[encoders.keys()].copy()
+
+    return x, mapping, df
+
+def build_edge_connections(time_matrix_path: str, start_times: np.array, finish_times: np.array, delimiter=';'):
+    matrix = np.loadtxt(time_matrix_path, delimiter=delimiter)
+    # Add the transport times (matrix) to the finishing time of every task
+    transport_times = matrix + np.expand_dims(finish_times, axis=-1)
+
+    possible_connection = np.where(transport_times <= start_times)
+
+    return torch.tensor(np.array(possible_connection))
+
+# Normalize and convert dataframe column (Series) into a tensor
+class NumberNormEncoder:
+    def __init__(self, dtype=None):
+        self.dtype = dtype
+
+    def __call__(self, df):
+        return torch.from_numpy(((df - df.mean()) / df.std()).values).view(-1, 1).to(self.dtype)
+
+def load_graph(path: set):
+    nodes, mapping, df = load_node_csv(f'{path}/tasks.csv', 'Index', 
+                               {name: NumberNormEncoder() for name in ['ZastavkaStart', 'ZastavkaFinish', 'CasStart', 'CasFinish', 'Vzdialenost']}, 
+                               sep=';')
+
+    edge_connections = build_edge_connections(f'{path}/Tij.csv', df['CasStart'].values, df['CasFinish'].values)
+
+    data = Data(x=nodes, edge_index=edge_connections)
+
+    add_state = T.Constant(0)
+
+    return add_state(data)
